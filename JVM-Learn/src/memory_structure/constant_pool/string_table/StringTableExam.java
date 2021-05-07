94caab10fbf953e4ded8a387d9d6d294f084e46c
GILDATA\zhuzw <zhuzw@gildata.com>
2021-05-07 13:37:18 +0800

    字符串常量池测试程序完善

--- JVM-Learn/src/memory_structure/constant_pool/string_table/StringTableExam.java
+++ JVM-Learn/src/memory_structure/constant_pool/string_table/StringTableExam.java
@@ -8,10 +8,42 @@ package memory_structure.constant_pool.string_table;
  * @Version: 1.0
  */
 public class StringTableExam {
+
+    private static void test() {
+        String s1 = "a";
+        String s2 = "b";
+        String s3 = "ab";
+        String s4 = s1 + s2;
+        String s5 = "a" + "b";
+        String s6 = s4. intern();
+
+        System.out.println(s3 == s4);   //false
+        System.out.println(s3 == s5);   //true
+        System.out.println(s3 == s6);   //true
+
+
+
+        //问：如果在x2.intern()之前判断
+        //如果是jdk1.6呢
+        String y2 = new String("x") + new String("y");  //new String("xy")
+        String y3 = y2.intern();
+        String y1 = "xy";
+        System.out.println(y1 == y3);   //true
+        //1.6返回false，因为y2.intern()会创建一个新的""xy对象并返回
+
+        //=========jdk1.6==========
+        //String y2 = new String("x") + new String("y");  //new String("xy")
+        //y2.intern();    //串池不存在，放入复制的副本
+
+        //String y1 = "xy";   //获取到的是y2复制的副本
+        //System.out.println(y2 == y1);   //false  intern时不存在，成功放入
+    }
+
+
     public static void main(String[] args) {
 
         test();
-        int i = 1 / 0;
+
         String s1 = "a";
         String s2 = "b";
         String s3 = "ab";
@@ -47,37 +79,4 @@ public class StringTableExam {
         //System.out.println(y2 == y1);   //false  intern时不存在，成功放入
     }
 
-    private static void test() {
-        String s1 = "a";
-        String s2 = "b";
-        String s3 = "ab";
-        String s4 = s1 + s2;
-        String s5 = "a" + "b";
-        String s6 = s4. intern();
-
-        System.out.println(s3 == s4);   //false
-        System.out.println(s3 == s5);   //true
-        System.out.println(s3 == s6);   //true
-
-        //========================
-        String x2 = new String("c") + new String("d");  //new String("cd")
-        String x1 = "cd";   // "cd"
-        x2.intern();
-        //问：如果在x2.intern()之前判断
-        //如果是jdk1.6呢(交换后)
-        System.out.println(x1 == x2);   //false intern时已存在，放不进
-
-        //===========交换========
-        String y2 = new String("x") + new String("y");  //new String("xy")
-        y2.intern();
-        String y1 = "xy";
-        System.out.println(y2 == y1);   //true
-
-        //=========jdk1.6==========
-        //String y2 = new String("x") + new String("y");  //new String("xy")
-        //y2.intern();    //串池不存在，放入复制的副本
-
-        //String y1 = "xy";   //获取到的是y2复制的副本
-        //System.out.println(y2 == y1);   //false  intern时不存在，成功放入
-    }
 }
